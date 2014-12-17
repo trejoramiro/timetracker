d964c89499b0e4e9cf6ec7ab3b48e9a07bbd8634
Ramiro Trejo <trejoramiro@yahoo.com>
2014-12-17 00:29:52 -0600

    Add the first file.

---
+++ timetracker.py
@@ -0,0 +1,28 @@
+#timetracker.py
+#a command line timetracker for project managment.
+#knowing how long a project takes; ideal for workacholics and software engineers.
+#track multiple projects.
+
+#start, pause, exit, save, update, close, terminate
+
+import time
+import datetime
+import calendar 
+
+#epoch = time.gmtime(0)
+
+#first = time.time()
+
+#timing algorithms function
+#timing = time.clock()
+
+#Enter your project's name
+project = input("Enter project name: ")
+
+#Start the timer for that project
+
+#Record the time
+
+
+
+#print(timing)
